f90de570bc7c809fad893fb85820d95147e756fb
Michael Betz <michibetz@gmail.com>
2023-09-28 23:06:59 +0200

    pcb ordered from jlc

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ void setup()
 	pinMode(6, INPUT_PULLUP);
 
 	TCCR2A = _BV(WGM21) | _BV(WGM20) | _BV(COM2B1);
-	TCCR2B = _BV(WGM22) | _BV(CS20);
+	TCCR2B = _BV(WGM22) | _BV(CS20);  // only CS20 = no prescaler
 	OCR2A = 0xFF;
 	OCR2B = 1;
 
@@ -47,50 +47,11 @@ void setup()
 
 void loop()
 {
-	// static int pwr = 0;
+	static int pwr = 0;
 	static int tick = 0;
-	// static bool is_auto = true;
+	static bool is_auto = true;
 
 	delay(50);
-
-	double n1 = pnoise1d(0.0002 * tick++, 0.7, 5, seed1);
-	n1 = n1 * n1 * 120;
-
-	// if (is_auto)
-	// 	pwr = n1;
-
-	// int c = Serial.read();
-	// if (c == -1) {
-	// 	set_power(pwr);
-	// 	return;
-	// } else if (c == '=' && pwr < 0xFF) {
-	// 	pwr++;
-	// } else if (c == '-' && pwr > 0) {
-	// 	pwr--;
-	// } else if (c == '0') {
-	// 	pwr = 0;
-	// } else if (c == '2' && OCR2A > 0) {
-	// 	OCR2A--;
-	// } else if (c == '1' && OCR2A < 0xFF) {
-	// 	OCR2A++;
-	// } else if (c == 'q') {
-	// 	is_auto = !is_auto;
-	// }
-
-	// Serial.print("pwr: ");
-	// Serial.print(pwr);
-
-	// Serial.print("   f: ");
-	// Serial.print(OCR2A);
-
-	// Serial.print("\n");
-
-	set_power(n1);
-	// set_power(0xFF);
-
-	Serial.print(n1);
-	Serial.print("   ");
-	Serial.println(OCR2B);
 
 	if (digitalRead(6) == 0) {
 		OCR2B = 0;
@@ -98,4 +59,36 @@ void loop()
 		Serial.print("ARC!\n");
 		delay(5000);
 	}
+
+	double n1 = pnoise1d(0.0002 * tick++, 0.7, 5, seed1);
+	n1 = n1 * n1 * 120;
+
+	if (is_auto)
+		pwr = n1;
+
+	int c = Serial.read();
+	if (c == -1) {
+		set_power(pwr);
+		return;
+	} else if (c == '=' && pwr < 0xFF) {
+		pwr++;
+	} else if (c == '-' && pwr > 0) {
+		pwr--;
+	} else if (c == '0') {
+		pwr = 0;
+	} else if (c == '2' && OCR2A > 0) {
+		OCR2A--;
+	} else if (c == '1' && OCR2A < 0xFF) {
+		OCR2A++;
+	} else if (c == 'q') {
+		is_auto = !is_auto;
+	}
+
+	Serial.print("pwr: ");
+	Serial.print(pwr);
+
+	Serial.print("   f: ");
+	Serial.print(OCR2A);
+
+	Serial.print("\n");
 }
