e57989fa7b8724fe447c15eabd3650b89d78b2f1
Michael Betz <michibetz@gmail.com>
2023-10-01 15:22:37 +0200

    generate glitch-free phase shifted drive signals

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include<Arduino.h>
 
+#define MAX_PWR 0xF0  // need dead-zone near TOP to avoid glitches
+
 #define PIN_PWM_A 6  // OC0A
-#define PIN_PWM_B 9  // OC1A
+#define PIN_PWM_B 5  // OC0B
+// #define PIN_PWM_B 9  // OC1A
+// #define PIN_PWM_B 10  // OC1B
 #define PIN_AD 8  // Arc detector
+
+void set_phase(uint8_t v)
+{
+	if (v > MAX_PWR)
+		v = MAX_PWR;
+	cli();
+	while(TCNT0 <= MAX_PWR) {}
+	OCR0B = v;
+	sei();
+}
 
 void setup()
 {
@@ -10,19 +24,31 @@ void setup()
 	pinMode(PIN_PWM_B, OUTPUT);
 	pinMode(PIN_AD, INPUT);
 
-	TCCR0A = _BV(COM0A1) | _BV(WGM00);  // Phase correct, fixed frequency
+	// Timer 0: toggle at fixed frequency and phase
+	OCR0A = 0;
+	OCR0B = 0;
+	TCCR0A = _BV(COM0B0) | _BV(COM0A0);  // TOP = 0xFF
 	TCCR0B = _BV(CS00);  // only CS00 = no prescaler
-
-	TCCR1A = _BV(COM1A1) | _BV(WGM10);  // Phase correct, fixed frequency
-	TCCR1B = _BV(CS10);  // only CS10 = no prescaler
 
 	Serial.begin(115200);
 	Serial.print("Yo! This is Bzzzz2!\n");
+
+	set_phase(0x30);
 }
 
 void loop()
 {
-	Serial.println(OCR0A++);
-	OCR1A++;
-	delay(10000);
+	static uint8_t val = 0;
+	int c = Serial.read();
+	if (c == -1) {
+		return;
+	}
+	if (c == '=' && val < MAX_PWR) {
+		val++;
+	} else if (c == '-' && val > 0) {
+		val--;
+	}
+	set_phase(val);
+
+	Serial.println(OCR0B);
 }
